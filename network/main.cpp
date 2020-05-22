cb5b67556c3fe505546395af83dfd04bdaae3ef2
jiewang35 <jiewang35@iflytek.com>
2020-05-22 10:40:54 +0800

    输出

--- network/main.cpp
+++ network/main.cpp
@@ -1,8 +1,9 @@
 ﻿#include <QCoreApplication>
-#
+#include <QDebug>
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+    qDebug()<<"hello centos";
     return 0;
     return a.exec();
 }
